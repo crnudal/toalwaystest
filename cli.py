9a8750ede8ca3b3792ac11d3d6fc2ba473f12796
crnudal <121486059+crnudal@users.noreply.github.com>
2026-01-22 13:24:51 +0200

    Update cli.py

--- cli.py
+++ cli.py
@@ -18,3 +18,7 @@ def pytest_configure(config):
     """Store CLI options as global variables."""
     pytest.table_name = config.getoption("--table-name")
     pytest.where_clause = config.getoption("--where-clause")
+
+
+TABLE_NAME = pytest.table_name if hasattr(pytest, 'table_name') else "my_table"
+WHERE_CLAUSE = pytest.where_clause if hasattr(pytest, 'where_clause') else "id < 1000"
